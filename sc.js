b88f931769c000b799c060bfc03f046d8a5f1e16
Olek <roguezilla0@gmail.com>
2019-10-01 10:13:09 +0100

    remove debug shit

--- sc.js
+++ sc.js
@@ -5,10 +5,7 @@ $(document).ready(function() {
         click: function() {
             document.getElementById("div1").innerHTML = "";
             $.get("https://cors-anywhere.herokuapp.com/" + "https://api-v2.soundcloud.com/resolve?url={0}&client_id={1}".format($("#url_input").val(), client_id), function(data) {
-                //console.log("ID: " + data["id"]);
                 $.get("https://cors-anywhere.herokuapp.com/" + "https://api.soundcloud.com/i1/tracks/{0}/streams?client_id={1}".format(data["id"], client_id), function(data) {
-                    //console.log("Song url: " + data["http_mp3_128_url"]);
-                    
                     makeLink("Song", data["http_mp3_128_url"]);         
                 });             
             });
