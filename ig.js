caa160be3d611acaf724d756fd2ddbf211dd85eb
Olek <roguezilla0@gmail.com>
2019-10-01 11:29:48 +0100

    yes

--- ig.js
+++ ig.js
@@ -5,16 +5,14 @@ $(document).ready(function() {
                 document.getElementById("div1").innerHTML = "";
                 try {
                     for(match of regexInput(/"display_url":(".*?")/g, data)) {
-                        let link = encodeURI(match.substr('"display_url":"'.length, match.length).slice(0, -1));
-                        link = link.replaceAll('%5Cu0026', '&');
+                        let link = encodeURI(match.substr('"display_url":"'.length, match.length).slice(0, -1)).replaceAll('%5Cu0026', '&');
                         makeLink("Picture", link);
                     }
                 } catch (error) {}
                 
                 try {
                     for(match of regexInput(/"video_url":(".*?")/g, data)) {
-                        let link = encodeURI(match.substr('"video_url":"'.length, match.length).slice(0, -1));
-                        link = link.replaceAll('%5Cu0026', '&');
+                        let link = encodeURI(match.substr('"video_url":"'.length, match.length).slice(0, -1)).replaceAll('%5Cu0026', '&');
                         makeLink("Video", link);
                     }
                 } catch(error) {}
